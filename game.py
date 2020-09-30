b9d05f6f04489b67ee40e8f6efd459126bb9b21c
vagrant <vagrant@vagrant.vm>
2020-09-30 23:40:39 +0000

    Almost there

--- game.py
+++ game.py
@@ -13,33 +13,31 @@ name = input('<type in your name> ')
 while True:
     guess = input('What is your guess? ')
 
+    # convert the input string into an integer
+    g = int(guess)
+
     # check if the input is a valid number
-    if ! int(guess):
+    """if int(guess) == ValueError:
         print("Not a valid number")
-        continue
-    """try:
-        guess = int(guess)
-    except ValueError:
-        print(f'"{guess}" is not a valid number, try again.')
         continue
     """
 
     # check if the guess is between 1 and 100
-    if guess < 1 and guess > 100:
+    if g < 1 and g > 100:
         print('Please guess a number between 1 and 100')
         continue
 
     # increment the counter
     count += 1  
 
-    if guess < random_number:
+    if g < random_number:
         print('Your guess is too low.')
 
-    elif guess > randon_number:
+    elif g > random_number:
         print('Your guess is too high.')
 
     else:
         print(f'Yay, {name}!')
-        print(f'You guessed the number in {tries} tries!')
+        print(f'You guessed the number in {count} tries!')
         break
 
